069ee7919971f826088cfea735d6e623801610eb
GaBaljaintheroom <junsu1222@naver.com>
2022-12-02 18:19:25 +0900

    Todo 수정

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -12,27 +12,54 @@ import React, { useState } from "react";
 function Todo(props) {
 
   const [item, setItem] = useState(props.item);
+  const [readOnly, setReadOnly] = useState(true);
   
   const deleteItem = props.deleteItem;
+  const editItem = props.editItem;
 
   //deleteEventHandler 작성
   const deleteEventHandler = () => {
     deleteItem(item);
   }
 
+  const turnOffReadOnly = () => {
+    setReadOnly(false);
+  }
+
+  const turnOnReadOnly = (e) => {
+    if (e.key === "Enter") {
+      setReadOnly(true);
+    }
+  }
+
+  const editEventHandler = (e) => {
+    item.title = e.target.value;
+    editItem();
+  }
+
+  const checkboxEventHandler = (e) => {
+    item.done = e.target.checked;
+    editItem();
+  }
+
     return (
         <ListItem>
-            <Checkbox checked={item.done} />
+            <Checkbox checked={item.done} onChange={checkboxEventHandler} />
             <ListItemText>
                 <InputBase 
-                    inputProps={{"aria-label": "naked"}}
-                    type="text"
-                    id={item.id}
-                    name={item.id}
-                    value={item.title}
-                    multiline={true}
-                    fullWidth={true}
-                    />
+            inputProps={{
+              "aria-label": "naked",
+              readOnly: readOnly}}
+            onClick={turnOffReadOnly}
+            onKeyDown={turnOnReadOnly}
+            onChange={editEventHandler}
+            type="text"
+            id={item.id}
+            name={item.id}
+            value={item.title}
+            multiline={true}
+            fullWidth={true}
+            />
         </ListItemText>
         <ListItemSecondaryAction>
           <IconButton aria-label="Delete Todo"
